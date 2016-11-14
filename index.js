058579b71f3c144d366e3a65916c95956f7cfce1
李志文 <lizhiwen@meizu.com>
2016-11-14 15:06:42 +0800

    set "loose": false

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const babel = require('babel-core');
+const presets = {
+    'latest': require('babel-preset-latest'),
+    'es2015': require('babel-preset-es2015'),
+    'react': require('babel-preset-react'),
+    'stage-0': require('babel-preset-stage-0'),
+    'stage-1': require('babel-preset-stage-1'),
+    'stage-2': require('babel-preset-stage-2'),
+    'stage-3': require('babel-preset-stage-3')
+};
 
 module.exports = function (content, file, settings) {
     // 添加 useBabel 配置项，如果 useBabel 为 false 则不进行编译
@@ -10,7 +19,7 @@ module.exports = function (content, file, settings) {
         presets: [
             ["latest", {
                 "es2015": {
-                    "loose": true
+                    "loose": false
                 }
             }],
             "react",
@@ -24,6 +33,21 @@ module.exports = function (content, file, settings) {
             ext: '.html',
             isHtmlLike: true
         });
+    }
+
+    // 为了不需要在项目目录内安装 babel-presets 依赖
+    if (Array.isArray(settings.presets)) {
+        let presetName, i;
+
+        for (i = 0; i < settings.presets.length; i++) {
+            presetName = settings.presets[i];
+
+            if (Array.isArray(presetName) && presets[presetName[0]]) {
+                settings.presets[i][0] = presets[presetName[0]];
+            } else if (presets[presetName]) {
+                settings.presets[i] = presets[presetName];
+            }
+        }
     }
 
     const result = babel.transform(content, settings);
